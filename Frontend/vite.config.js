d45d63ed5892622000c9063bd8224126733d1c92
Manisha Soni <manishasoni@startbitsolutions.com>
2025-05-22 11:23:21 +0530

    added vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+ 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3005
+  },
+  define: {
+    'process.env.Base_URL': JSON.stringify('https://realestateadminpanel-2.onrender.com')
+  }
 })
+ 
