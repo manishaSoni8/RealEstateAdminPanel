535f6cc6d6a775f0c114957cfbb0814fcc0e268e
BalajiPathak <balajipathak@startbitsolutions.com>
2025-05-23 11:47:43 +0530

    upload images added

--- Backend/routes/blog.js
+++ Backend/routes/blog.js
@@ -20,23 +20,23 @@ router.get('/blogs', blogController.getAllBlogs);
 router.get('/blogs/:id', blogController.getBlogById);
 router.post('/blogs/comment', upload.single('Img'), blogController.addComment);
 
-// Protected routes (any authenticated user)
+// Admin routes (only token verification needed)
 router.post(
     '/blogs/create', 
-    blogController.verifyToken,
+    blogController.verifyToken, // Changed from verifyAdmin
     upload.single('Img'), 
     blogController.createBlog
 );
 
 router.delete(
     '/blogs/:id', 
-    blogController.verifyToken,
+    blogController.verifyToken, // Changed from verifyAdmin
     blogController.deleteBlog
 );
 
 router.put(
     '/blogs/:id',
-    blogController.verifyToken,
+    blogController.verifyToken, // Changed from verifyAdmin
     upload.single('Img'),
     blogController.editBlog
 );
