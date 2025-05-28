0522055f29c4fcc755192b4979ddef13e45049f1
Manisha Soni <manishasoni@startbitsolutions.com>
2025-05-28 14:45:24 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,21 +7,21 @@ export default defineConfig({
     strictPort: false,
     port: 5173,
     allowedHosts: [
-      'realestateadminpanel-3.onrender.com',
-      'realestateadminpanel-2.onrender.com'
+      'realestateadminpanel-1-ioxn.onrender.com',
+      'realestateadminpanel-45v0.onrender.com'
     ],
     cors: true
   },
   define: {
-    'process.env.VITE_BASE_URL': JSON.stringify('https://realestateadminpanel-2.onrender.com')
+    'process.env.VITE_BASE_URL': JSON.stringify('https://realestateadminpanel-45v0.onrender.com')
   },
   preview: {
     port: 3005,
     host: true,
     strictPort: true,
     allowedHosts: [
-      'realestateadminpanel-3.onrender.com',
-      'realestateadminpanel-2.onrender.com'
+      'realestateadminpanel-1-ioxn.onrender.com',
+      'realestateadminpanel-45v0.onrender.com'
     ]
   }
 })
